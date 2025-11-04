b91f57856b56c64483ad67fbcfdf7c7f9bfbd351
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2025-11-04 15:19:57 +0000

    chore: match bullet names with regex

--- BetterBulletNames/PatchItemDisplayName.cs
+++ BetterBulletNames/PatchItemDisplayName.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using ItemStatsSystem;
 using HarmonyLib;
 
@@ -7,15 +7,37 @@ namespace BetterBulletNames
     [HarmonyPatch(typeof(Item), nameof(Item.DisplayName), MethodType.Getter)]
     public class PatchItemDisplayName
     {
-        private static Dictionary<string, string> displayNameConversions = new Dictionary<string, string> {
-            { "Standard Bullet (AR)", "(AR) Standard" }
-        };
-
         static void Postfix(Item __instance, ref string __result)
         {
-            if (displayNameConversions.ContainsKey(__result))
+            UnityEngine.Debug.Log($"Item.DisplayName: {__result}");
+
+            var matchWithCaliber = Regex.Match(__result, @"^(.+?)\s+Bullet\s*\(([^)]+)\)\s*$");
+            if (matchWithCaliber.Success)
             {
-                __result = displayNameConversions[__result];
+                UnityEngine.Debug.Log($"matchWithCaliber: {matchWithCaliber.Groups[0].Value}");
+                string prefix = matchWithCaliber.Groups[1].Value.Trim();
+                string caliber = matchWithCaliber.Groups[2].Value.Trim();
+
+                __result = $"({caliber}) {prefix}";
+                return;
+            }
+
+            var matchSniper = Regex.Match(__result, @"^(.+?)\s+Sniper Bullet$");
+            if (matchSniper.Success)
+            {
+                UnityEngine.Debug.Log($"matchSniper: {matchSniper.Groups[0].Value}");
+                string prefix = matchSniper.Groups[1].Value.Trim();
+
+                __result = $"(Snip) {prefix}";
+            }
+
+            var matchShotgun = Regex.Match(__result, @"^(.+?)\s+Shotgun Shell$");
+            if (matchShotgun.Success)
+            {
+                UnityEngine.Debug.Log($"matchShotgun: {matchShotgun.Groups[0].Value}");
+                string prefix = matchShotgun.Groups[1].Value.Trim();
+
+                __result = $"(Shot) {prefix}";
             }
         }
     }
